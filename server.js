0ef6acb979a9b10f2fd0ff4422cd129090ef3c09
Jim Chi <jimcinbrisbane@pm.me>
2020-11-23 11:26:32 +1000

    change to random challange

--- server.js
+++ server.js
@@ -6,15 +6,29 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient;
 const test = require('assert');
 // Connection url
-const url = 'mongodb://localhost:27017';
 // Database Name
-const dbName = 'a';
+
+const url = 'mongodb+srv://jim:115825477q.@digiclub.p6qw3.mongodb.net/digiClub?retryWrites=true&w=majority';
+// Database Name
+const database = "digiClub";
 // Connect using MongoClient
-
+const helmet = require("helmet");
 app.use(express.json())
-const companyRouter = require('./routes/company')
-app.use('/company', companyRouter)
-app.use('/data', express.static('data'))
-app.use(express.static('public'))
-app.listen(3000,() => console.log("server started at localhost:3000"))
+app.use(helmet());
 
+app.get('/unit',(req, res) => {
+  MongoClient.connect(url, function(err, db) {
+    if(err) { 
+        console.log('Unable to connect to the mongoDB server. Error:', err)
+    } else { 
+        db.db("digiClub").collection('units').aggregate([
+          {$sample: { size: 1 } }
+        ]).toArray(function(err, results) {
+            console.log(results);
+            res.json(results);
+        });
+    };
+})})
+app.use(express.static('public'))
+const port = process.env.PORT || 8080
+app.listen(port,() => console.log("server started at localhost:8080"))
